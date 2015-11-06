7bc94a65243900c1adb88d5825f195c9dc0e2de5
zaifoski <sofiachimirri@gmailcom>
2015-11-07 00:28:35 +0100

    add list of cats you can choose between

--- cat-clicker/app.js
+++ cat-clicker/app.js
@@ -1,20 +1,34 @@
-var tmpl = "<div class=\"cat\">"+
+var tmpl1 = "<li class=\"cats-list-item\" id=\"ID\">"+
+           "NAME"+
+           "</li>"
+var tmpl2 = "<div class=\"cat\">"+
            "<h3 class=\"counter\">0</h3>"+
-           "<img class=\"catimage\" id=\"ID\" src=\"IMG\">"+
+           "<img class=\"catimage\" id=\"ID-img\" src=\"IMG\">"+
            "</div>"
 
 $(document).ready(function(){
     
     for(var i=0; i< data.length; i++){
-        var item = tmpl.replace("ID",data[i].id);
+        var item = tmpl1.replace("ID",data[i].id);
+        item = item.replace("NAME",data[i].id);
+        $(".cats-list").append(item);
+        
+        var item = tmpl2.replace("ID",data[i].id);
         item = item.replace("IMG",data[i].img);
         $(".cats").append(item);
     }
+    $(".cat").hide();
     $(".catimage").on("click", function (event) {
         var el = $(this).parent(".cat").find("h3");
         var c = el.html();
         c++;
         el.html(c);
+    });
+    $(".cats-list-item").on("click", function (event) {
+        $(".cat").hide();
+        var identifier = $(this).attr('id');
+        console.log(identifier);
+        $('#'+identifier+"-img").parent(".cat").show();
     });
 });
 
--- cat-clicker/data.js
+++ cat-clicker/data.js
@@ -6,5 +6,17 @@ var data = [
 {
  id : "cat2",
  img : "img/cat2.jpg"
-}                 
+}, 
+{
+ id : "cat3",
+ img : "img/cat3.jpg"
+}, 
+{
+ id : "cat4",
+ img : "img/cat4.jpg"
+}, 
+{
+ id : "cat5",
+ img : "img/cat5.jpg"
+}            
 ];
